76d1ee48044a603cbadbdf0b2bf44610f5ccf38b
kaioumdev <afrankaiyum34@gmail.com>
2026-08-01 07:10:46 +0600

    feat(provider): implements provider to the main page

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
+import './index.css'
+import 'remixicon/fonts/remixicon.css'
+import { RouterProvider } from 'react-router-dom'
+import router from './routes/router.jsx'
+import { store } from './redux/store.js'
+import { Provider } from 'react-redux'
+import 'sweetalert2/dist/sweetalert2.js'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <Provider store={store}>
+    <RouterProvider router={router} />
+  </Provider>,
 )
